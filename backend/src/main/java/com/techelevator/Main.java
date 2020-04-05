ff1eba601590c109ee8f35d1a56e138626a89bb3
David P-C <david.pc@techelevator.com>
2020-04-05 14:43:08 -0400

    Added starter projects for Vue and Spring Boot.

---
+++ backend/src/main/java/com/techelevator/Main.java
@@ -0,0 +1,45 @@
+package com.techelevator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.techelevator.authentication.JwtAuthInterceptor;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class Main implements WebMvcConfigurer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Main.class, args);
+	}
+
+	@Bean
+	public BasicDataSource dataSource() {
+		BasicDataSource dataSource = new BasicDataSource();
+		dataSource.setDriverClassName("org.postgresql.Driver");
+		dataSource.setUrl("jdbc:postgresql://localhost:5432/userdb");
+		dataSource.setUsername("postgres");
+		dataSource.setPassword("postgres1");
+		return dataSource;
+	}
+	
+	@Bean
+	public JwtAuthInterceptor interceptor() {
+		List<String> exceptions = new ArrayList<String>();
+		exceptions.add("/register");
+		exceptions.add("/login");
+		return new JwtAuthInterceptor(exceptions);
+	}
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(interceptor());
+	}
+
+}
---
+++ backend/src/test/java/com/techelevator/MainTests.java
@@ -0,0 +1,14 @@
+package com.techelevator;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
