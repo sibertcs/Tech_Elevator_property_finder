a3c57f355b07646b1772ceaec91fd6385c715c65
Drew Cook <drewcook1321@gmail.com>
2020-04-06 14:24:20 -0400

    backend sql

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -3,11 +3,19 @@ BEGIN TRANSACTION;
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
-  id serial PRIMARY KEY,
-  username varchar(255) NOT NULL UNIQUE,     -- Username
+  user_id serial PRIMARY KEY,
+  email varchar(255) NOT NULL UNIQUE,     -- Email
+  first_name varchar(255) NOT NULL,
+  last_name varchar(255) NOT NULL,
+  phone_number varchar(10) NOT NULL,
   password varchar(32) NOT NULL,      -- Password
   salt varchar(256) NOT NULL,          -- Password Salt
-  role varchar(255) NOT NULL default('user')
+  role varchar(255) NOT NULL default('renter'),
+  CHECK (role IN ('renter', 'landlord', 'maintenance', 'admin'))
 );
 
+--INSERT INTO users (email, first_name, last_name, phone_number, password, )
 COMMIT TRANSACTION;
+
+SELECT * FROM users;
+
