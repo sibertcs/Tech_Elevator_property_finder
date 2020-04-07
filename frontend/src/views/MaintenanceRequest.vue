fd7df02881969916ed5d2fe7b0052f82768befd3
Evan White <elevanwhite@gmail.com>
2020-04-07 11:44:14 -0400

    maintenance request form bones

--- frontend/src/views/MaintenanceRequest.vue
+++ frontend/src/views/MaintenanceRequest.vue
@@ -1,13 +1,26 @@
 <template>
-  <h1>This is the maintenance request page</h1>
+  <div>
+      <h1>This is the maintenance request page</h1>
+     
+    <form>
+      <input type="hidden" id="unitId" name="unitId" value="unitId !!!"/>
+      <label for="details">Details</label>
+      <textarea id="details"  rows="3" ></textarea>
+      <br>
+      <label for="priority">Priority Level</label>
+      <select id="priority">
+        <option>Low</option>
+        <option>Medium</option>
+        <option>High</option>
+      </select>
+      <button type="submit">Submit Request </button>
+    </form>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
