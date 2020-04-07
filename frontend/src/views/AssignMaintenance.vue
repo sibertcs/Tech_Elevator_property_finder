3f0ae5d2da91d9ec850c3a70ec5b3dedfa499c77
Rebecca Fox <18rulloa@gmail.com>
2020-04-07 11:31:54 -0400

    Assign Maintenance HTML

--- frontend/src/views/AssignMaintenance.vue
+++ frontend/src/views/AssignMaintenance.vue
@@ -1,5 +1,45 @@
 <template>
+<div>
   <h1>this is the page to assign maintenance</h1>
+  <div>
+    <table>
+      <tr>
+        <td>Requesting Resident: </td>
+        <td> Resident Name!!!! </td>
+      </tr> 
+      <tr>
+        <td>Unit: </td>
+        <td>Unit number!!!! </td>
+      </tr>  
+      <tr>
+        <td>Urgency: </td>
+        <td>Urgency Level!!! </td>
+      </tr> 
+      <tr>
+        <td>Request Date: </td>
+        <td>Date!!! </td>
+      </tr> 
+      <tr>
+        <td>Assigned to: </td>
+        <td>Maintenance Name!!! </td>
+      </tr> 
+      <tr>
+        <td>Status: </td>
+        <td>isCompleted?!!! </td>
+      </tr>
+      </table>   
+      <form>
+        <label>Assign Maintenance Employee: </label>
+        <select>
+        <option>Employee 1!!! </option>
+        <option>Employee 2!!! </option>
+        <option>Employee 3!!! </option>
+         </select>
+      </form>
+
+    
+  </div>  
+</div>
 </template>
 
 <script>
