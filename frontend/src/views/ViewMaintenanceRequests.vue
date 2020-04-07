62fdb0e92f761c74677be70c348672d722942128
Rebecca Fox <18rulloa@gmail.com>
2020-04-07 11:22:25 -0400

    view maintenance Requests HTML

--- frontend/src/views/ViewMaintenanceRequests.vue
+++ frontend/src/views/ViewMaintenanceRequests.vue
@@ -1,5 +1,35 @@
 <template>
+<div>
   <h1>This is all the Maintenance Requests</h1>
+  <div>
+    <table>
+      <tr>
+        <td>Requesting Resident: </td>
+        <td> Resident Name!!!! </td>
+      </tr> 
+      <tr>
+        <td>Unit: </td>
+        <td>Unit number!!!! </td>
+      </tr>  
+      <tr>
+        <td>Urgency: </td>
+        <td>Urgency Level!!! </td>
+      </tr> 
+      <tr>
+        <td>Request Date: </td>
+        <td>Date!!! </td>
+      </tr> 
+      <tr>
+        <td>Assigned to: </td>
+        <td>Maintenance Name!!! </td>
+      </tr> 
+      <tr>
+        <td>Status: </td>
+        <td>isCompleted?!!! </td>
+      </tr> 
+    </table>  
+  </div>  
+</div>
 </template>
 
 <script>
