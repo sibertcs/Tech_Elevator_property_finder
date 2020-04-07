fcdf138177dd60d3dbd5931805b10a066e259e98
Evan White <elevanwhite@gmail.com>
2020-04-07 10:37:00 -0400

    payment html bones

--- frontend/src/views/Payment.vue
+++ frontend/src/views/Payment.vue
@@ -1,11 +1,42 @@
 <template>
-  <h1>This is the payment page</h1>
+  <div>
+    <h1>This is the payment page</h1>
+    <div>
+      <table>
+        <tr>
+          <td>Property ID:</td>
+          <td>123!!!</td>
+        </tr>
+        <tr>
+          <td>Amount Due:</td>
+          <td>$100!!!</td>
+        </tr>
+        <tr>
+          <td>Amount Paid:</td>
+          <td>$0!!!</td>
+        </tr>
+        <tr>
+          <td>Due Date:</td>
+          <td>01/01/2020!!!</td>
+        </tr>
+        <tr>
+          <td>Status:</td>
+          <td>Unpaid!!!</td>
+        </tr>
+      </table>
+    </div>
+    <div>
+     <form>
+       <label for="payment">Make a Payment </label>
+       <input id="payment" type="text" placeholder="$$$"/>
+       <button type="submit">Submit Payment</button>
+     </form>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
