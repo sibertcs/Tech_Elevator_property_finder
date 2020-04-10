5b88064809dc1715e80334fb2fe4c34d1d0f0cfa
Zachary Kenney <zkenney417@gmail.com>
2020-04-10 09:37:06 -0400

    merging

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -279,6 +279,10 @@ INSERT INTO payment (rent_cycle_id, amount_paid, date_paid) VALUES (6, 1429, '20
 INSERT INTO payment (rent_cycle_id, amount_paid, date_paid) VALUES (9, 1429, '2020-03-31 12:00:00');
 
 
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
 COMMIT TRANSACTION;
 
 --SELECT * FROM property;
@@ -288,3 +292,9 @@ COMMIT TRANSACTION;
 SELECT * FROM property;
 
 SELECT * FROM unit;
+<<<<<<< Updated upstream
+=======
+
+Update unit Set is_available = false WHERE unit_id = 12;
+
+>>>>>>> Stashed changes
