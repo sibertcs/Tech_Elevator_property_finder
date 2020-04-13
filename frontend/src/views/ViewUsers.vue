a05d3d146f2d0c03b2e7af97fabee311fe876cf0
Zachary Kenney <zkenney417@gmail.com>
2020-04-13 09:44:45 -0400

    View Users fetch

--- frontend/src/views/ViewUsers.vue
+++ frontend/src/views/ViewUsers.vue
@@ -3,8 +3,32 @@
 </template>
 
 <script>
-export default {
+import auth from '../auth';
 
+export default {
+  data() {
+    return {
+      users: []
+    }
+  },
+  created() {
+    fetch("http://localhost:8080/api/users", {
+        method: 'GET',
+          headers: {
+            Authorization: 'Bearer ' + auth.getToken(),
+            'Content-Type': 'application/json'
+          },
+          credentials: 'same-origin'
+    })
+    .then(response => {
+      if(response.ok) {
+        return response.json();
+      }
+    })
+    .then(responseData => {
+      this.users = responseData;
+    })
+  }
 }
 </script>
 
