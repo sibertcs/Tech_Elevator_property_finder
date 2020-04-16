366e124b7481c77880cee602222e6be03b9a4d3b
Angie Auel <angieauel415@gmail.com>
2020-04-16 16:02:03 -0400

    fixed header on Register

--- frontend/src/views/Register.vue
+++ frontend/src/views/Register.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="register" class="text-center container is-fluid">
     <form class="form-register" @submit.prevent="register">
-      <h1 class="h3 mb-3 font-weight-normal">Create Account</h1>
+      <h1 class="title">Create Account</h1>
       <div class="alert alert-danger" role="alert" v-if="registrationErrors">
         There were problems registering this user.
       </div>
