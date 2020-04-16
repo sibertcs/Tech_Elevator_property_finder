452292f9c6e382f21215b59d42c7788fa244cf8e
Angie Auel <angieauel415@gmail.com>
2020-04-16 11:40:51 -0400

    Order and wording of menu options

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -19,11 +19,11 @@
           <div class=navDiv v-if="loggedIn">
             <b-navbar-dropdown :label="`${roleName} Menu`" v-if="user != undefined">
               <div v-if="user != undefined && (user.rol=== 'renter' || user.rol==='admin')">
-                <b-navbar-item href="#">
-                  <router-link to="/payment">Payment</router-link>
-                </b-navbar-item>
                 <b-navbar-item>
                   <router-link to="/lease">Lease Info</router-link>
+                </b-navbar-item>
+                <b-navbar-item href="#">
+                  <router-link to="/payment">Rent Payments</router-link>
                 </b-navbar-item>
                 <b-navbar-item>
                   <router-link :to="{name:'request'}">Maintenance Request</router-link>
@@ -32,16 +32,16 @@
 
               <div v-if="user != undefined && (user.rol=== 'landlord' || user.rol==='admin')">
                 <b-navbar-item href="#">
-                  <router-link :to="{name: 'tenants'}">View Tenants/Landlord</router-link>
+                  <router-link :to="{name: 'addOrUpdate'}">Add/Update Properties</router-link>
                 </b-navbar-item>
                 <b-navbar-item href="#">
                   <router-link :to="{name: 'addOrUpdateLease'}">Add/Update Lease</router-link>
                 </b-navbar-item>
                 <b-navbar-item href="#">
-                  <router-link :to="{name: 'assignMaintenance'}">Assign Maintenance</router-link>
+                  <router-link :to="{name: 'tenants'}">View Tenants</router-link>
                 </b-navbar-item>
                 <b-navbar-item href="#">
-                  <router-link :to="{name: 'addOrUpdate'}">Add/Update Properties</router-link>
+                  <router-link :to="{name: 'assignMaintenance'}">Assign Maintenance</router-link>
                 </b-navbar-item>
               </div>
               <div v-if="user.rol=== 'maintenance' || user.rol==='admin'">
